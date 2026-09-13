945e28d5922ceff7896e190858f47f3ad2cc5e38
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2026-09-13 21:46:11 +0700

    1st working codeset

---
+++ src/content.config.ts
@@ -0,0 +1,23 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const blog = defineCollection({
+  loader: glob({
+    pattern: "**/*.md",
+    base: "./docs",
+  }),
+
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.coerce.date(),
+    category: z.string(),
+    tags: z.string(),
+    slug: z.string(),
+  }),
+});
+
+export const collections = {
+  blog,
+};
