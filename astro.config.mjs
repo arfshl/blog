dd465733b549d43a26dfc94ca43736f8ca2eb9c0
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2026-09-15 23:24:01 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   site: 'https://arfshl.github.io',
-  base: command === 'build' ? '/blog' : '',
+  base: '/blog',
   vite: {
-    plugins: [tailwindcss()],
-  },
-}));
+    plugins: [tailwindcss()]
+  }
+});
