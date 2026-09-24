5a2ae711033203c8b8ef057d4fb6252d39964179
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2026-09-24 13:24:58 +0700

    now links will be opened in new tab

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,37 @@
 import { defineConfig } from 'astro/config';
-import { unified, rehypeHeadingIds } from '@astrojs/markdown-remark';
+import { unified } from '@astrojs/markdown-remark';
 import tailwindcss from '@tailwindcss/vite';
+import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
   site: 'https://arfshl.github.io',
   base: '/blog',
+
   markdown: {
     processor: unified({
       rehypePlugins: [
-        rehypeHeadingIds,
+        rehypeSlug,
+
         [
           rehypeAutolinkHeadings,
           {
-            behavior: 'wrap',
+            behavior: 'append',
+          },
+        ],
+
+        [
+          rehypeExternalLinks,
+          {
+            target: '_blank',
+            rel: ['noopener'],
           },
         ],
       ],
     }),
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
